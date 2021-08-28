ec4ae82ea67ae4a1ace18d279bc22c692c0d9583
rajonahmed749 <raazon749@gmail.com>
2021-08-28 22:46:49 +0600

    another section added to homepage

---
+++ src/Components/home/Work.js
@@ -0,0 +1,38 @@
+import { Button, Modal } from 'antd';
+import React, { useState } from 'react';
+
+const Work = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+    return (
+        <div id="works" className="block worksBlock">
+            <div className="container-fluid">
+                <div className="titleHolder">
+                    <h2>How it works</h2>
+                    <p>Perspiciatis vero similique, ut ducimus modi ipsam autem tempora</p>
+                </div>
+                <div className="contentHolder">
+                    <Button onClick={showModal}>
+                        <i class="fas fa-play"></i>
+                    </Button>
+                </div>
+                <Modal title="Classical romantic song" footer={null} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                    <iframe width="460" height="315" src="https://www.youtube.com/embed/gvyUuxdRdR4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </Modal>
+            </div>
+        </div>
+    );
+};
+
+export default Work;
